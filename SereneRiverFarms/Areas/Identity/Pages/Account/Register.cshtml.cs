25cbf7953720affb51476a70b795b7f33545cd97
logan <logan.testa@outlook.com>
2019-11-11 21:35:05 -0800

    Adjust comments on register

--- SereneRiverFarms/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SereneRiverFarms/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,14 +81,18 @@ namespace SereneRiverFarms.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password");
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmal",
-                        pageHandler: null,
-                        values: new { userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+
+
 
                     //I commented this out since I'm not using it.  
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmal",
+                    //    pageHandler: null,
+                    //    values: new { userId = user.Id, code = code },
+                    //    protocol: Request.Scheme);
+
                     //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     //$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
