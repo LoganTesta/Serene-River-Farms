ccf2b9216427094e7f647ca1c98cdb1426bb815b
logan <logan.testa@outlook.com>
2019-11-14 14:16:04 -0800

    Add delete account client side validation

---
+++ SereneRiverFarms/wwwroot/assets/javascript/delete-account-validation.js
@@ -0,0 +1,62 @@
+﻿
+/* JavaScript Contact Form Validation. */
+let clickedSubmit = false;
+
+
+function validateDeleteAccountForm() {
+    if (clickedSubmit) {
+        let userPassword = $("#userPassword").val().trim();
+        let validForm = true;
+        
+
+        let validPassword = true;
+        if (userPassword === null || userPassword === "") {
+            validPassword = false;
+        }
+
+        /* Check password length. */
+        if (userPassword.length < 7) {
+            validPassword = false;
+        }
+
+        if (validPassword) {
+            $("#userPassword").removeClass("required-field-needed");
+        } else {
+            validForm = false;
+            $("#userPassword").addClass("required-field-needed");
+        }
+
+
+        if (validForm === false) {
+            $(".javascript-validation-results-contact-us").eq(0).addClass("show");
+            $(".javascript-validation-results-contact-us").eq(0).html("Please fill all required fields in the correct format.");
+            return false;
+        } else if (validForm) {
+            $("javascript-validation-results-contact-us").eq(0).removeClass("show");
+            $("javascript-validation-results-contact-us").eq(0).html("");
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+
+function setClickedDeleteAccountButtonTrue() {
+    let elementWithFocus = $(':focus')[0];
+    if (deleteAccountButton === elementWithFocus) {
+        clickedSubmit = true;
+    }
+}
+
+$("#deleteAccountButton").on("click", function () {
+    setClickedDeleteAccountButtonTrue();
+});
+
+$("#deleteAccountButton").on("click", function () {
+    validateDeleteAccountForm()
+});
+
+$("#userPassword").on("change", function () {
+    validateDeleteAccountForm();
+});
