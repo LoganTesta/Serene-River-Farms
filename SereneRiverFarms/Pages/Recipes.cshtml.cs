08ffd6a1866f601541443e48ea64a4b34cbf520a
logan <logan.testa@outlook.com>
2020-01-10 17:06:51 -0800

    Add beginning of recipes page

--- SereneRiverFarms/Pages/Recipes.cshtml.cs
+++ SereneRiverFarms/Pages/Recipes.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,13 +7,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SereneRiverFarms.Pages
 {
-    public class AboutModel : PageModel
+    public class RecipesModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet()
         {
-            Message = "Your application description page.";
         }
     }
 }
