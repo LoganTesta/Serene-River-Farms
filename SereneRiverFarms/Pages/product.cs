8a4cba19fb1fb6c3587a950d81941137667cc82a
logan <logan.testa@outlook.com>
2020-05-28 18:24:53 -0700

    Add classInt, displayCSS to products

--- SereneRiverFarms/Pages/product.cs
+++ SereneRiverFarms/Pages/product.cs
@@ -7,16 +7,19 @@ public class product
     public string name { get; set; }
     public string namePlural { get; set; }
     public string classCSS { get; set; }
+    public string classInt { get; set; }
+    public string displayCSS { get; set; }
     public decimal price { get; set; }
     public string category { get; set; }
     public string image { get; set; }
     public string description { get; set; }
 
-    public product(string name, string namePlural, string classCSS, decimal price, string category, string image, string description)
+    public product(string name, string namePlural, string classCSS, string classInt, string displayCSS, decimal price, string category, string image, string description)
     {
         this.name = name;
         this.namePlural = namePlural;
         this.classCSS = classCSS;
+        this.classInt = classInt;
         this.price = Math.Round(price, 2, MidpointRounding.AwayFromZero);
         this.category = category;
         this.image = image;
